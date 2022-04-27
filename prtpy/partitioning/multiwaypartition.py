46a339944875e04b941686e0066c0200a3314f9a
Nadav Suissa <74867230+nadavsuissa@users.noreply.github.com>
2022-04-27 14:26:13 +0300

    Add files via upload

--- prtpy/partitioning/multiwaypartition.py
+++ prtpy/partitioning/multiwaypartition.py
@@ -1,4 +1,13 @@
-import doctest
+"""
+Algorithm for an Optimal solution to the multiway number partitioning problem
+
+Based on: Search Strategies for Optimal Multi-Way Number Partitioning Michael D. Moffitt
+
+https://www.ijcai.org/Proceedings/13/Papers/099.pdf
+"""
+import logging
+logger = logging.getLogger(__name__)
+
 # Function to check if all subsets are filled or not
 def validateS(sumLeft:list, k:int)-> bool:
     return r
@@ -12,56 +21,41 @@ def sSum(S, n, sumLeft, A, k)-> bool:
 
 # Function for solving k–partition problem. It prints the subsets if
 # set `S[0…n-1]` can be divided into `k` subsets with equal sum
-def kwaypartition(S:list, k:int)-> list:
+def multiwaypartition(
+        S:list,
+        k:int)-> list:
     """
     Main Function - Given Set on Int and K PArtiotions 
     :param S: Set of Integers
     :param k: Int - Num of Partitions
     :return: List of K Lists partiotions of sets that match Criteria
 
-    >>> kwaypartition([2,3,4,5,1,2,4], 3)
+    Examples that have been precalculated:
+
+    >>> multiwaypartition([2,3,4,5,1,2,4], 3)
     [[2, 1, 4], [2, 5], [3, 4]]
-    >>> kwaypartition([4,3,6,8,2,4,9], 3)
+    >>> multiwaypartition([4,3,6,8,2,4,9], 3)
     [[3, 9], [4, 8], [4, 6, 2]]
-    >>> kwaypartition([5,3,6,8], 2)
+    >>> multiwaypartition([5,3,6,8], 2)
     [[6, 5], [8, 3]]
-    >>> kwaypartition([5,3,6,8,2,2,8], 2)
+    >>> multiwaypartition([5,3,6,8,2,2,8], 2)
     [[8, 3, 6], [8, 2, 2, 5]]
-    >>> kwaypartition([5,3,6,7], 1)
+    >>> multiwaypartition([5,3,6,7], 1)
     [5,3,6,7]
-    >>> kwaypartition([1,2,3,4], 2)
+    >>> multiwaypartition([1,2,3,4], 2)
     [[3, 2], [4, 1]]
-    >>> kwaypartition([4,4,4,4,4,4,4,4,4], 3)
+    >>> multiwaypartition([4,4,4,4,4,4,4,4,4], 3)
     [[4, 4, 4], [4, 4, 4], [4, 4, 4]]
-    >>> kwaypartition([4,2,3,7,4,5,9,2,4,9,1,2,9,7,4,7,8,3,2,3,4,5,7,1,3,1,1,1,1,1], 10)
+    >>> multiwaypartition([4,2,3,7,4,5,9,2,4,9,1,2,9,7,4,7,8,3,2,3,4,5,7,1,3,1,1,1,1,1], 10)
     [[1, 2, 9], [1, 2, 9], [3, 4, 5], [3, 9], [2, 7, 3], [1, 3, 8], [1, 2, 5, 4], [1, 7, 4], [1, 7, 4], [1, 7, 4]]
     """
-    return [4]
-
-
-
-
-
-
-
-""" 
-Author - Search Strategies for Optimal
- Multi-Way Number Partitioning
- Michael D. Moffitt
-
- Number Of Algorithms - 1 the Last One
-
- What Doe the Algrothm Do - Recives Set of Numbers and K Partitions - Returns Optimal Partitions such that all sums are the same
-
-
- HomeWork Check List:
- Arguments and Type - Done
- Type Returned - Done
- Documentation - Done
- Empty Implement - Done
- Failed DocTest - Done
-"""
+    return []
 
 if __name__ == '__main__':
-    doctest.testmod()
+    logger.addHandler(logging.StreamHandler())
+    # logger.setLevel(logging.INFO)
+
+    import doctest
+    (failures, tests) = doctest.testmod(report=True)
+    print("{} failures, {} tests".format(failures, tests))
 
